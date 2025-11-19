0d289c78536baef974fb78f024a19079994f65f6
sabarishravi93 <81555633+sabarishravi93@users.noreply.github.com>
2025-11-19 09:30:43 +0530

    Update outputs.tf

---
+++ terraform/env/dev/outputs.tf
@@ -0,0 +1,39 @@
+output "db_instance_id" {
+  description = "RDS instance identifier"
+  value       = module.rds.db_instance_id
+}
+
+output "db_instance_arn" {
+  description = "ARN of the RDS instance"
+  value       = module.rds.db_instance_arn
+}
+
+output "db_instance_endpoint" {
+  description = "Connection endpoint for the RDS instance"
+  value       = module.rds.db_instance_endpoint
+}
+
+output "db_instance_address" {
+  description = "Address of the RDS instance"
+  value       = module.rds.db_instance_address
+}
+
+output "db_instance_port" {
+  description = "Port of the RDS instance"
+  value       = module.rds.db_instance_port
+}
+
+output "db_name" {
+  description = "Name of the database"
+  value       = module.rds.db_name
+}
+
+output "db_security_group_id" {
+  description = "Security group ID attached to the RDS instance"
+  value       = module.rds.db_security_group_id
+}
+
+output "db_subnet_group_name" {
+  description = "Name of the DB subnet group"
+  value       = module.rds.db_subnet_group_name
+}
